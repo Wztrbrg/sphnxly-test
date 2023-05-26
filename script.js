2c916d10a1130ee46da33b29691b1d7460176b5f
Jonathan Andersson <2jonand@gmail.com>
2023-05-26 14:06:40 +0200

    added login logic

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const modal = document.querySelector(".login-modal");
 const modalLoginBtn = document.querySelector(".nav-login-btn");
 const closeBtn = document.querySelector(".close");
+const userInfo = document.querySelector(".user-info");
+
+const handleLogin = () => {
+  let username = document.querySelector(".username-field").value;
+  localStorage.setItem("username", username);
+  modal.classList.remove("show");
+  console.log(localStorage);
+  setUserInfo();
+};
+
+const setUserInfo = () => {
+  let username = localStorage.getItem("username");
+  if (localStorage) userInfo.innerHTML = "Du är inloggad som " + username;
+};
 
 //Show login modal when top nav login button is clicked
 modalLoginBtn.addEventListener("click", () => {
