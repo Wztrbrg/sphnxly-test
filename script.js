2ed5f2bef4e65fe1ee776172b113dfca95de5da4
Jonathan Andersson <2jonand@gmail.com>
2023-05-27 02:41:49 +0200

    Styling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector(".login-modal");
 const modalLoginBtn = document.querySelector(".nav-login-btn");
 const closeBtn = document.querySelector(".close");
+const userMessage = document.querySelector(".user-message");
 const userInfo = document.querySelector(".user-info");
 
 //Initialize current user
@@ -18,27 +19,39 @@ if (localStorage.getItem("users")) {
 //stores current user in users array
 //stores array in localStorage and closes modal
 const handleLogin = () => {
-  currentUser = document.querySelector(".username-field").value;
+  currentUser = document.querySelector(".username-field").value.toUpperCase();
   setUserInfo();
   users.push(currentUser);
   localStorage.setItem("users", JSON.stringify(users));
   modal.classList.remove("show");
 };
 
+const handleLogout = () => {
+  modalLoginBtn.innerText = "LOGGA UT";
+  modalLoginBtn.classList.add("logout");
+  modalLoginBtn.addEventListener("click", () => {
+    location.reload();
+  });
+};
 //Checking if current user exists in localStorage or not
 //and setting welcome message accordingly
 const setUserInfo = () => {
   if (users.includes(currentUser)) {
-    userInfo.innerHTML =
-      "Välkommen tillbaka! Du är inloggad som " + currentUser;
+    userMessage.innerHTML = "VÄLKOMMEN TILLBAKA! DU ÄR INLOGGAD SOM ";
+    userInfo.innerHTML = currentUser;
+    handleLogout();
   } else {
-    userInfo.innerHTML = "Du är inloggad som " + currentUser;
+    userMessage.innerHTML = "DU ÄR INLOGGAD SOM ";
+    userInfo.innerHTML = currentUser;
+    handleLogout();
   }
 };
 
 //Show login modal when top nav login button is clicked
 modalLoginBtn.addEventListener("click", () => {
-  modal.classList.add("show");
+  if (!modalLoginBtn.classList.contains("logout")) {
+    modal.classList.add("show");
+  }
 });
 
 //Hide login modal when login modal close button is clicked
